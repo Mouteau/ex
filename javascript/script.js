156e15461a90d7aebf7a940a186d64d863a14ee4
Antoine <antoine@Juliens-MacBook-Pro.local>
2020-04-05 15:53:10 +0200

    debut projet

---
+++ javascript/script.js
@@ -0,0 +1,25 @@
+var bonjour=true
+var timer
+document.getElementById("but").addEventListener("click",()=>{
+    console.log("click toto",bonjour)
+
+    if(bonjour===true)
+    {
+        document.getElementById("presentation").innerHTML="au revoir"
+        bonjour=false
+    }
+    else
+    {
+        document.getElementById("presentation").innerHTML="rebonjour"
+        bonjour=true
+    }
+
+    var i=0
+    window.clearInterval(timer)
+    timer=window.setInterval(()=>{
+        document.getElementById("presentation").innerHTML=i
+        i=i+1
+    },1000)
+    
+
+})
